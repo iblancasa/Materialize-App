be6a16fb849a25111d339ccd465d6daa40001342
Israel Blancas <iblancasa@gmail.com>
2015-05-17 12:19:34 +0200

    Adding toolbar #1

--- app/src/main/java/com/androidvigo/cloud/MainActivity.java
+++ app/src/main/java/com/androidvigo/cloud/MainActivity.java
@@ -2,6 +2,9 @@ package com.androidvigo.cloud;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -14,6 +17,8 @@ public class MainActivity extends ActionBarActivity
 
     private ListView mMemesListView;
     private ProgressBar mLoadingProgressBar;
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +26,40 @@ public class MainActivity extends ActionBarActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
         mMemesListView      = (ListView) findViewById(R.id.activity_main_memes_listview);
         mLoadingProgressBar = (ProgressBar) findViewById(R.id.activity_main_loading_indicator);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+      /*  if (id == R.id.action_settings) {
+            return true;
+        }*/
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
 
     public void startGetMemesRequest(View requestButton) {
 
